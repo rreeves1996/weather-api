2cc2d07680416fa2aaae76f0b85a6c4c0896b9b9
Ryan Reeves <reeves0ryan@gmail.com>
2023-01-24 18:35:57 -0800

    useFetch added to homepage search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,10 @@ import { BsFillCloudMoonFill } from 'react-icons/bs';
 import '../assets/style/home.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 export default function Home() {
+	const { fetchCoordinates, fetchLocation } = useFetch();
 	const [searchState, setSearchState] = useState('');
 	const navigate = useNavigate();
 
@@ -17,39 +19,23 @@ export default function Home() {
 	const handleSearchSubmit = async (event) => {
 		event.preventDefault();
 
-		const query = searchState.trim();
+		const search = searchState.trim();
 
-		if (query) {
-			axios
-				.get(
-					// Geolocater gets the requested city's latitude and longitude
-					`https://geocoding-api.open-meteo.com/v1/search?name=${query}`
-				)
-				.then((res) => {
-					const lat = res.data.results[0].latitude;
-					const lon = res.data.results[0].longitude;
-					const current = {
-						city: res.data.results[0].name,
-						state: res.data.results[0].admin1,
-					};
-					// Format timezone into URL friendly format
-					const unformattedTime = res.data.results[0].timezone.split('/');
-					const timezone = `${
-						unformattedTime[0] ? unformattedTime[0] : unformattedTime
-					}${unformattedTime[0] && `%2F${unformattedTime[1]}`}`;
+		if (search) {
+			await fetchCoordinates(search).then((res) => {
+				console.log(res.data);
+				const current = {
+					city: res.data.results[0].name,
+					state: res.data.results[0].admin1,
+				};
+				fetchLocation(res).then((res) => {
+					current.time = res.data.current_weather.time;
 
-					axios
-						.get(
-							`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,windspeed_80m,winddirection_80m,shortwave_radiation_instant&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=${timezone}`
-						)
-						.then((res) => {
-							current.time = res.data.current_weather.time;
-
-							navigate('/dashboard', {
-								state: { current: current, data: res.data },
-							});
-						});
+					navigate('/dashboard', {
+						state: { current: current, data: res.data },
+					});
 				});
+			});
 		}
 
 		setSearchState('');
